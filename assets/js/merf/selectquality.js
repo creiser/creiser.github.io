34f996ff06aa42870d6fcd780c12be7c1c427c96
Christian Reiser <christianreiser@gmx.net>
2024-10-19 08:38:48 +0200

    MERF: changed path to scene files hosted on google cloud storage

--- assets/js/merf/selectquality.js
+++ assets/js/merf/selectquality.js
@@ -1,7 +1,8 @@
 
 function start() {
   const params = new URL(window.location.href).searchParams;
-  dirUrl = '/assets/scenes/' + params.get('scene') + '.json';
+  //dirUrl = '/assets/scenes/' + params.get('scene') + '.json'; // for dropbox
+  dirUrl = 'https://storage.googleapis.com/realtime-nerf-360/merf/' + params.get('scene');
   const qualityPresets = ['phone', 'low', 'medium', 'high'];
   for (const quality of qualityPresets) {
     console.log(quality);
